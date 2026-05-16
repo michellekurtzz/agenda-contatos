888223051209bc6bd8fdd4e1669004778407e07f
Michelle Kurtz <kurtzmichelle290@gmail.com>
2026-05-16 15:59:16 -0300

    feat: implement edit and delete contacts

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from contacts.utils import show_menu
-from contacts.contact_manager import add_contact, list_contacts
+from contacts.contact_manager import (
+    add_contact,
+    list_contacts,
+    edit_contact,
+    delete_contact
+)
 
 while True:
     show_menu()
-    
+
     option = input("Escolha uma opção: ")
 
     if option == "1":
@@ -11,6 +16,12 @@ while True:
 
     elif option == "2":
         list_contacts()
+
+    elif option == "3":
+        edit_contact()
+
+    elif option == "6":
+        delete_contact()
 
     elif option == "7":
         print("Encerrando...")
--- contacts/contact_manager.py
+++ contacts/contact_manager.py
@@ -37,3 +37,30 @@ def list_contacts():
             f"Email: {contact['email']} | "
             f"Favorito: {favorite_status}"
         )
+
+
+def edit_contact():
+    name = input("Qual contato deseja editar? ")
+
+    for contact in contacts_list:
+        if contact["name"] == name:
+            contact["name"] = input("Novo nome: ")
+            contact["phone"] = input("Novo telefone: ")
+            contact["email"] = input("Novo email: ")
+
+            print("Contato editado com sucesso")
+            return
+
+    print("Contato não encontrado")
+
+
+def delete_contact():
+    name = input("Qual contato deseja deletar? ")
+
+    for contact in contacts_list:
+        if contact["name"] == name:
+            contacts_list.remove(contact)
+            print("Contato deletado com sucesso")
+            return
+
+    print("Contato não encontrado")
