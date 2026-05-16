e8a55126e2d669e80b283346ecf2086557a7ce10
Michelle Kurtz <kurtzmichelle290@gmail.com>
2026-05-16 15:44:35 -0300

    feat: create project structure and menu

---
+++ app.py
@@ -0,0 +1,10 @@
+from contacts.utils import show_menu
+
+while True:
+    show_menu()
+    
+    option = input("Escolha uma opção: ")
+
+    if option == "7":
+        print("Encerrando...")
+        break
---
+++ contacts/utils.py
@@ -0,0 +1,8 @@
+def show_menu():
+    print("1 - Adicionar contato")
+    print("2 - Ver contatos")
+    print("3 - Editar contato")
+    print("4 - Favoritar contato")
+    print("5 - Ver favoritos")
+    print("6 - Deletar contato")
+    print("7 - Sair")
